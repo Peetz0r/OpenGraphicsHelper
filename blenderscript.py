b5ad665832ca8d5a78064c286348d81fa2f6c9f7
Peetz0r <peter@haas-en-berg.nl>
2020-07-09 09:33:57 +0200

    Text output readability

--- blenderscript.py
+++ blenderscript.py
@@ -2,23 +2,26 @@ import bpy, os, io, sys, glob, json
 
 from contextlib import redirect_stdout
 
+def echo(s):
+  print(' ➡ %s' % s, flush=True)
+
 models = os.scandir('models')
 
 for model in models:
   if model.is_dir():
     obj = json.load(open(glob.glob('objects/**/*.'+model.name+'.json', recursive=True)[0]))
-    print(' ➡ %s (%s)' % (obj['strings']['name']['en-GB'], model.name))
+    echo('%s (%s)' % (obj['strings']['name']['en-GB'], model.name))
 
     if 'cars' in obj['properties']:
       for idx_car, car in enumerate(obj['properties']['cars']):
-        print(' ➡ car %d/%d' % (idx_car, len(obj['properties']['cars'])))
+        echo('car %d/%d' % (idx_car, len(obj['properties']['cars'])))
 
         bpy.ops.wm.open_mainfile(filepath='Blender-RCT-Graphics/lighting_rig.blend')
 
         bpy.data.scenes[0].rct_graphics_helper_general_properties.output_directory = '%s/output/car-%d/' % (model.path, idx_car)
 
         fbx_file = '%s/car-%d.fbx' % (model.path, idx_car)
-        print(' ➡ importing %s' % (fbx_file))
+        echo('importing %s' % (fbx_file))
 
         with redirect_stdout(open(os.devnull, 'w')):
           bpy.ops.import_scene.fbx(filepath=fbx_file)
@@ -43,11 +46,11 @@ for model in models:
         bpy.ops.render.rct_vehicle()
 
     else:
-      print(' ➡ no cars')
+      echo('no cars')
       bpy.ops.wm.open_mainfile(filepath='Blender-RCT-Graphics/lighting_rig.blend')
 
       fbx_file = glob.glob(model.path+'/*.fbx')[0]
-      print(' ➡ importing %s' % (fbx_file))
+      echo('importing %s' % (fbx_file))
 
       with redirect_stdout(open(os.devnull, 'w')):
         bpy.ops.import_scene.fbx(filepath=fbx_file)
